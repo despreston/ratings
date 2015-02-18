5c75cc68d39d69643236b536fddc6b6f36ceafdd
Des Preston <dpreston@enservio.com>
2015-02-18 17:06:07 -0500

    Serve up static content

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ var express = require('express'),
     controllers = require('./controllers')(app, parser),
     routes = require('./routes')(app, controllers);
 
+app.use(express.static(__dirname + '/ratings_static'));
+
 app.locals.members = [];
 
 var server = app.listen(process.env.PORT || 3000, function() {
