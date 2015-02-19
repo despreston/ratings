fc7f3d173cae1a6a16ab9b2fd6d270153c984330
Des Preston <dpreston@enservio.com>
2015-02-18 19:16:18 -0500

    Moving to cors package

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 var express = require('express'),
+    cors = require('cors'),
     app = express();
 
-app.use(function(req, res, next) {
+app.use(cors());
+
+/*app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     next();
-});
+});*/
 
 var parser = require('./parser'),
     controllers = require('./controllers')(app, parser),
