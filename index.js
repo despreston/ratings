0439694b1cd9addf0070c0e6be57a5fef41fb8fe
despreston <despreston@gmail.com>
2015-03-27 14:02:40 -0400

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ app.all(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('test-header', 'fuck this needs to work');
     return next();
 });
 app.use(express.static(__dirname + '/ratings_static'));
