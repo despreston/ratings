fb24b872cda9d657135929f830ec787e323f5fc3
Desmond Preston <despreston@gmail.com>
2016-03-31 17:52:14 -0400

    more files

---
+++ src/server.js
@@ -0,0 +1,20 @@
+var express = require('express'),
+    cors = require('cors'),
+    app = express(),
+    parser = require('./parser'),
+    controllers = require('./controllers')(app, parser),
+    routes = require('./routes')(app, controllers);
+
+app.all(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    return next();
+});
+app.use(express.static(__dirname + '/ratings_static'));
+
+app.locals.members = [];
+
+var server = app.listen(process.env.PORT || 3000, function() {
+    console.log("listening for requests");
+});
